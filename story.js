8fa796f49007c92b37d1934c0efe20aa19e5b208
elephent <elephent@xj.com>
2016-08-01 10:40:18 +0800

    lase week work

---
+++ story.js
@@ -0,0 +1,179 @@
+// JavaScript Document
+function two(){
+var a1=document.getElementById("title1");
+a1.innerHTML="大话西游之大圣娶亲";
+var b1=document.getElementById("img1");
+b1.src="dahuaxiyouzhidashengquqin.jpg";
+var c1=document.getElementById("introduce1")
+c1.innerHTML="至尊宝（周星驰）被月光宝盒带回到五百年前，遇见紫霞仙子（朱茵），被对方打上烙印成为对方的人，并发觉自己已变成孙悟空。 紫霞与青霞（朱茵）本是如来佛祖座前日月神灯的灯芯（白天是紫霞，晚上是青霞），二人虽然同一肉身却仇恨颇深，因此紫霞立下誓言，谁能拔出她手中的紫青宝剑，谁就是她的意中人。紫青宝剑被至尊宝于不经意间拔出，紫霞决定以身相许，却遭一心记挂白晶晶（莫文蔚）的至尊宝拒绝。后牛魔王救下迷失在沙漠中的紫霞，并逼紫霞与他成婚，关键时刻，至尊宝现身。 ";
+var d1=document.getElementById("begintime1")
+d1.innerHTML="2014-10-24(中国大陆) / 1995-02-04(香港)"
+
+var a2=document.getElementById("title2");
+a2.innerHTML="大话西游之月光宝盒";
+var b2=document.getElementById("img2");
+b2.src="dahuaxiyouzhiyueguangbaohe.jpg";
+var c2=document.getElementById("introduce2")
+c2.innerHTML="孙悟空（周星驰）护送唐三藏（罗家英）去西天取经路上，与牛魔王合谋欲杀害唐三藏，并偷走了月光宝盒，此举使观音萌生将其铲除心思，经唐三藏请求，孙悟空被判五百年后重新投胎做人赎其罪孽。五百年后孙悟空化身强盗头头至尊宝。当遇见预谋吃唐僧肉的妖怪姐妹蜘蛛精春三十娘（蓝洁瑛）和白骨精白晶晶（莫文蔚）时，因为五百年前孙悟空曾与白晶晶有过一段恋情，至尊宝与她一见钟情，但因菩提老祖将二人妖怪身份相告，至尊宝仍带领众强盗开始与二妖展开周旋，过程中，白晶晶为救至尊宝打伤春三十娘，自己也中毒受伤，为了救白晶晶，至尊宝去找春三十娘，遭白晶晶误会，绝望自杀，至尊宝开始用月光宝盒以期使时光倒流。";
+var d2=document.getElementById("begintime2")
+d2.innerHTML=" 2014-10-24(中国大陆) / 1995-01-21(香港)"
+
+var a3=document.getElementById("title3");
+a3.innerHTML="功夫";
+var b3=document.getElementById("img3");
+b3.src="gongfu.jpg";
+var c3=document.getElementById("introduce3")
+c3.innerHTML="1940年代的上海，自小受尽欺辱的街头混混阿星（周星驰）为了能出人头地，可谓窥见机会的缝隙就往里钻，今次他盯上行动日益猖獗的黑道势力“斧头帮”，想借之大名成就大业。阿星假冒“斧头帮”成员试图在一个叫“猪笼城寨”的地方对居民敲诈，不想引来真的“斧头帮”与“猪笼城寨”居民的恩怨。“猪笼城寨”原是藏龙卧虎之处，居民中有许多身怀绝技者（元华、梁小龙等），他们隐藏于此本是为远离江湖恩怨，不想麻烦自动上身，躲都躲不及。而在观战正邪两派的斗争中，阿星逐渐领悟功夫的真谛。";
+var d3=document.getElementById("begintime3")
+d3.innerHTML="2004-12-23(中国大陆/香港) / 2015-01-15(中国大陆3D) / 2004-09-14(多伦多电影节)"
+
+var a4=document.getElementById("title4");
+a4.innerHTML="让子弹飞";
+var b4=document.getElementById("img4");
+b4.src="rangzidanfei.jpg";
+var c4=document.getElementById("introduce4")
+c4.innerHTML="民国年间，花钱捐得县长的马邦德（葛优 饰）携妻（刘嘉玲 饰）及随从走马上任。途经南国某地，遭劫匪张麻子（姜文 饰）一伙伏击，随从尽死，只夫妻二人侥幸活命。马为保命，谎称自己是县长的汤 师爷。为汤师爷许下的财富所动，张麻子摇身一变化身县长，带着手下赶赴鹅城上任。有道是天高皇帝远，鹅城地处偏僻，一方霸主黄四郎（周润发 饰）只手遮天，全然不将这个新来的县长放在眼里。张麻子痛打了黄的武教头（姜武 饰），黄则设计害死张的义子小六（张默 饰）。原本只想赚钱的马邦德，怎么也想不到竟会被卷入这场土匪和恶霸的角力之中。鹅城上空愁云密布，血雨腥风在所难免…… ";
+var d4=document.getElementById("begintime4")
+d4.innerHTML=" 2010-12-16(中国大陆)"
+
+var a5=document.getElementById("title5");
+a5.innerHTML="西游降魔篇";
+var b5=document.getElementById("img5");
+b5.src="xiyouxiangmopian.jpg";
+var c5=document.getElementById("introduce5")
+c5.innerHTML="大唐年间妖魔横行，一小渔村因为饱受鱼妖之害请来道士（冯勉恒 饰）除妖，年轻驱魔人陈玄奘（文章 饰）前来帮忙却被误认为骗子，幸亏职业赏金驱魔人段小姐（舒淇 饰）帮助玄奘制服了鱼妖真身（李尚正 饰）。二人又在高家庄为制服猪妖猪刚鬣（陈炳强 饰） 而再次相遇，这次除妖没有成功 ，但是段小姐却对玄奘二见钟情。玄奘求助师父，得知除妖的办法是去找被压在五指山下的孙悟空（黄渤 饰）帮忙，于是他准备前往五指山，途中又遇到段小姐和手下五煞，段小姐连蒙带哄想与玄奘在一起却屡次遭拒，在四妹（周秀娜 饰）调教下想变得更有女人味却适得其反。二人决裂后玄奘独自上路，与此同时降魔师（释延能 饰）、天残脚（张超理 饰）、空虚公子（罗志祥 饰）也一同前往除妖。经过千辛万苦玄奘终于找到孙悟空，段小姐又再次出现并交给玄奘一件重要的东西，猪妖终于被降服，但是更大的危机又出现在了玄奘面前，原...  ";
+var d5=document.getElementById("begintime5")
+d5.innerHTML=" 2010-12-16(中国大陆)"
+}
+
+function one(){
+var a1=document.getElementById("title1");
+a1.innerHTML="疯狂动物城";
+var b1=document.getElementById("img1");
+b1.src="img2.jpg";
+var c1=document.getElementById("introduce1")
+c1.innerHTML="故事发生在一个所有哺乳类动物和谐共存的美好世界中，兔子朱迪（金妮弗·古德温 Ginnifer Goodwin 配音）从小就梦想着能够成为一名惩恶扬善的刑警，凭借着智慧和努力，朱迪成功的从警校中毕业进入了疯狂动物城警察局，殊不知这里是大型肉食类动物的领地，作为第一只，也是唯一的小型食草类动物，朱迪会遇到怎样的故事呢？ ";
+var d1=document.getElementById("begintime1")
+d1.innerHTML="2016-03-04(中国大陆/美国)"
+
+var a2=document.getElementById("title2");
+a2.innerHTML="唐人街探案";
+var b2=document.getElementById("img2");
+b2.src="img3.jpg";
+var c2=document.getElementById("introduce2")
+c2.innerHTML="天赋异禀的结巴少年秦风（刘昊然 饰）警校落榜，被姥姥遣送泰国找远房表舅——号称“唐人街第一神探”，实则猥琐的大叔唐仁（王宝强 饰）散心。不想一夜花天酒地后，唐仁沦为离奇凶案嫌疑人，不得不和秦风亡命天涯……穷追不舍的警探黄兰登（陈赫 饰）、无敌幸运的警察坤泰（肖央 饰）、穷凶极恶的匪帮三人组、高深莫测的唐人街教父、美艳风骚的老板娘阿香（佟丽娅 饰）等悉数登场！七天！唐仁、秦风必须取长补短、同仇敌忾！他们能否躲避警察追捕、匪帮追杀、黑帮围剿，在短短七天内，完成找到失落的黄金并查明真凶，为自己洗清罪名这些逆天的任务？";
+var d2=document.getElementById("begintime2")
+d2.innerHTML=" 2015-12-31(中国大陆)"
+
+var a3=document.getElementById("title3");
+a3.innerHTML="寒战2";
+var b3=document.getElementById("img3");
+b3.src="img4.jpg";
+var c3=document.getElementById("introduce3")
+c3.innerHTML="冲锋车案件主谋李家俊（彭于晏 饰）设计逃脱警方控制，与幕后黑手汇合，联手策划更大阴谋。而现警务处长刘杰辉（郭富城 饰），前警务副处长李文彬（梁家辉 饰），和资深大律师简奥伟（周润发 饰）被卷入其中，香港将面临更加大的危机……";
+var d3=document.getElementById("begintime3")
+d3.innerHTML="2004-12-23(中国大陆/香港) / 2015-01-15(中国大陆3D) / 2004-09-14(多伦多电影节)"
+
+var a4=document.getElementById("title4");
+a4.innerHTML="大圣归来";
+var b4=document.getElementById("img4");
+b4.src="img5.jpg";
+var c4=document.getElementById("introduce4")
+c4.innerHTML="五百年前，由石猴变化而成的齐天大圣孙悟空（张磊 配音）大闹天宫，最终被如来佛祖镇在了五行山下。此去经年，长安城内突然遭到山妖洗劫，童男童女哭声连连，命悬一线。危机时刻，自幼被行脚僧法明（吴文伦 配音）抚养长大的江流儿（林子杰 配音）救下了一个小女孩，结果反遭山妖追杀。经过一番追逐，江流儿无意中解除了孙悟空的封印，齐天大圣自然好好地将山妖教训了一番。因为封印未解开，悟空不得不护送江流儿和小女孩回长安，一路上又遭遇了猪八戒（刘九容 配音）和白龙马。 与此同时，妖王（童自荣 配音）绝不甘心失败，他躲在暗中，等待着给孙悟空一行致命一击的良机…… ";
+var d4=document.getElementById("begintime4")
+d4.innerHTML=" 2015-07-10(中国大陆)"
+
+var a5=document.getElementById("title5");
+a5.innerHTML="三人行";
+var b5=document.getElementById("img5");
+b5.src="img8.jpg";
+var c5=document.getElementById("introduce5")
+c5.innerHTML="影片讲述了六小时内，一个医生——佟倩（赵薇 饰）、一个悍匪——张礼信（钟汉良 饰）、一个警察——陈伟乐（古天乐 饰），三方在“生、老、病、死”轮回不断的医院发生的惊心动魄的交锋。生死一线，自我执念引发诸多隐瞒、利用与欺骗，三人越走越偏，危机一触即发……";
+var d5=document.getElementById("begintime5")
+d5.innerHTML=" 2016-06-24(中国大陆) / 2016-06-30(香港)"
+}
+
+
+function ten(){
+var a1=document.getElementById("title1");
+a1.innerHTML="中国合伙人";
+var b1=document.getElementById("img1");
+b1.src="china companise.jpg";
+var c1=document.getElementById("introduce1")
+c1.innerHTML="20世纪80年代，三个怀有热情和梦想的年轻人在高等学府燕京大学的校园内相遇，从此展开了他们长达三十年的友谊和梦想征途。出生于留学世家的孟晓骏（邓超 饰）渴望站在美国的土地上改变世界，浪漫自由的王阳（佟大为 饰）尽情享受改革开放初期那蓬勃激昂的青春气息，曾两次高考落榜的农村青年成冬青（黄晓明 饰）以晓骏为目标努力求学，并收获了美好的爱情。然而三个好友最终只有晓骏获得美国签证，现实和梦想的巨大差距让冬青和王阳倍受打击。偶然机缘，被开除公职的冬青在王阳的帮助下办起了英语培训学校，开始品尝到成功的喜悦。在美国发展不顺的晓骏回国，并加入学校，无疑推动三个好友朝着梦想迈进了一大步。";
+var d1=document.getElementById("begintime1")
+d1.innerHTML="2013-05-17(中国大陆) / 2013-05-30(香港)"
+
+var a2=document.getElementById("title2");
+a2.innerHTML="港囧";
+var b2=document.getElementById("img2");
+b2.src="taijiong.jpg";
+var c2=document.getElementById("introduce2")
+c2.innerHTML="徐来（徐峥 饰）大学时代的理想是能够成为一名出色的油画家，他的坚持和不屈吸引了美丽女孩杨伊（杜鹃 饰）的注意，随着时间的推移，他们相爱了。然而，徐来的这段初恋却最终被残酷的现实所粉碎，之后，他同一直守候在自己身边的内衣公司老板千金蔡波（赵薇 饰）走到了一起。　毕业后，徐来和蔡波结婚，成了蔡家的“上门女婿”，他放弃了理想，开始经营内衣公司，尽管事业蒸蒸日上，但徐来的心中始终留存着一抹遗憾。某日，徐来意外得知如今已经成为画家的杨伊将在香港举行她的个展，心中五味杂陈的他带着一家老小前往香港，名为旅游，实则是相同杨伊“再续前缘”。然而，徐来的计划却屡屡遭到小舅子蔡拉拉（包贝尔 饰）的破坏，蔡拉拉正在拍摄一部家庭纪录片，在他的镜头背后，徐来发现了一个陌生的自己。";
+var d2=document.getElementById("begintime2")
+d2.innerHTML="2015-09-25(中国大陆)"
+
+var a3=document.getElementById("title3");
+a3.innerHTML="人在囧途";
+var b3=document.getElementById("img3");
+b3.src="renzaijiongtu.jpg";
+var c3=document.getElementById("introduce3")
+c3.innerHTML="大老板李成功（徐峥 饰）遇上挤奶工牛耿（王宝强 饰）之后，旅途便频出状况。被情人逼迫回长沙老家跟老婆摊牌的李成功，机场遭遇到前往长沙讨债的乌鸦嘴牛耿。牛耿人如其名，不但耿直憨厚，而且透出一股傻气。先是登机前安检一口气喝完一大罐牛奶，后来又飞机上让乘务员开窗，好不容易折腾到飞机到达长沙上空，结果让他咒得因长沙大雪飞机被迫返航。无奈挤上火车硬座车厢的李成功刚松了一口气，却又一次在人群中看到牛耿。牛耿就像李成功生命中的瘟神一样，只要他“金口一开”，便会出现如他所言的意外。由于途中的频频意外，两人从火车换乘巴士，又从巴士爬上拖拉机。尽管牛耿的乌鸦嘴让李成功吃尽苦头，但这个混身透着傻气的青年却用自己真诚与乐观感染着李成功。一路的颠簸之后，两人最终到达长沙又回到各自的生活轨迹中去，然而旅途中所遭遇的种种却影响着两人之后的生活……";
+var d3=document.getElementById("begintime3")
+d3.innerHTML="2010-06-04 (中国大陆)"
+
+var a4=document.getElementById("title4");
+a4.innerHTML="泰囧";
+var b4=document.getElementById("img4");
+b4.src="gangjiong .jpg";
+var c4=document.getElementById("introduce4")
+c4.innerHTML="　商业成功人士徐朗（徐峥 饰）用了五年时间发明了一种叫“油霸”的神奇产品——每次汽车加油只需加到三分之二，再滴入2滴“油霸”，油箱的汽油就会变成满满一箱。徐朗的同学，兼商业竞争对手高博（黄渤 饰）想把这个发明一次性卖给法国人。但徐朗坚决不同意，他希望深入开发研究， 把“油霸”发扬光大，得到更远的收益。两个人各抒己见，争论不休，一直无果。由于两人股份相同，唯有得到公司最大股东周扬的授权书，方可达到各自目的。当得知周扬在泰国后，徐朗立刻启程寻找。而高博获悉后将一枚跟踪器放在徐朗身上一起去了泰国。飞机上，徐朗遇到了王宝（王宝强 饰），别有心机地想利用他来摆脱对手高博的追赶，可他不仅没甩掉王宝，还成了他的“贴身保姆”…… ";
+var d4=document.getElementById("begintime4")
+d4.innerHTML=" 2012-12-12(中国大陆)"
+
+var a5=document.getElementById("title5");
+a5.innerHTML="美人鱼";
+var b5=document.getElementById("img5");
+b5.src="meirenyu.jpg";
+var c5=document.getElementById("introduce5")
+c5.innerHTML="白手起家的富豪刘轩（邓超 饰）新拍下了一块地皮，并联合了女强人李若兰（张雨绮 饰）使用恐怖的声纳技术驱赶鱼类，用于填海造地。人鱼一族长期居住在附近区域的海里，为了继续生存，带头大哥章鱼八哥（罗志祥 饰）派出了美人鱼珊珊（林允 饰）“色诱”刘轩，准备刺杀。没想到珊珊在卧底过程中与刘轩暗生情愫，一次次破坏暗杀计划，而李若兰却在准备一个更丧心病狂的邪恶计划…";
+var d5=document.getElementById("begintime5")
+d5.innerHTML=" 2016-02-08(中国大陆/香港)"
+}
+
+function first(){
+	one();	
+	
+	
+}
+function last(){
+	ten();	
+	
+}
+
+function next(){
+	if(document.getElementById("title1").textContent=="疯狂动物城")
+		{two();}
+	else if(document.getElementById("title1").textContent=="大话西游之大圣娶亲")
+	{ten();}	
+	else if(document.getElementById("title1").textContent=="中国合伙人")
+	{alert("没有下一页了")}
+	
+}
+function previous(){
+	if(document.getElementById("title1").textContent=="中国合伙人")
+		{two();}
+	else if(document.getElementById("title1").textContent=="大话西游之大圣娶亲")
+	{one();}	
+	else if(document.getElementById("title1").textContent=="疯狂动物城")
+	{alert("没有上一页了")}
+	
+}
+
+function skip()
+{
+	var ev=document.getElementById("page").value;
+	if(ev==1){one();}
+	if(ev=="2"){two();}
+	if(ev==10){ten();}	
+}
